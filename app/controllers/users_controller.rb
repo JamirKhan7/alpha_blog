3d9b56b2b9ffbaf53507475c603bd95a08e2c25f
JamirKhan7 <jamirkhan007@gmail.com>
2025-01-23 07:47:18 +0530

    [Users]: Signup and update profile

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = "Hello, #{@user.username}, Welcome to Alpha Blog, You have successfully signed up"
+      redirect_to articles_path
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your profile has been updated successfully."
+      redirect_to articles_path
+    else
+      render "edit"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+end
